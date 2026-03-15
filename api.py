42404c54269090e4024c8fe1c942497b8082cf0a
LINCHENYU2030S <peterway6933@gmail.com>
2026-03-16 01:15:04 +0800

    refine

--- api.py
+++ api.py
@@ -238,4 +238,4 @@ async def get_embedding(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", port=args.port, reload=False)
+    uvicorn.run(app, host='0.0.0.0', port=args.port, reload=False)
